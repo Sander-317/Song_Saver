b42fa4a7b0aa956f6b4572aa6321d55e71f59298
Sander-317 <sander.van.meerveld@gmail.com>
2023-01-19 12:22:13 +0100

    made the header look nice and added home and about button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h1>Son Saver</h1>
+      <br />
+      <button>home</button>
+      <button>about</button>
+    </div>
+  );
 }
