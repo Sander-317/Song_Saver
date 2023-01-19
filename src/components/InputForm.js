20bb8ac3d48226b091acfff4ec60c0f7e6f86b3e
Sander-317 <sander.van.meerveld@gmail.com>
2023-01-19 12:31:44 +0100

    added the input form

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function InputForm() {
-  return <div>InputForm</div>;
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="song" />
+        <input type="text" placeholder="artist" />
+        <select name="gerne">
+          <option value="rock">rock</option>
+          <option value="punk">punk</option>
+          <option value="disco">disco</option>
+        </select>
+        <button>add song</button>
+      </form>
+    </div>
+  );
 }
